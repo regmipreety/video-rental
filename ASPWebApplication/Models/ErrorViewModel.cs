8460105ea15f936480de613fc390dcf9803d3a30
regmipreety <priety.regmi46@gmail.com>
2024-09-28 10:58:53 -0500

    initial files

---
+++ ASPWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
