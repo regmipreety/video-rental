fbb8fd6800715b2cfb85e104a13fdab4603eff30
regmipreety <priety.regmi46@gmail.com>
2024-09-29 11:56:02 -0500

    Add Connection to Database and create category

---
+++ ASPWebApplication/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPWebApplication.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		[DisplayName("Category Name")]
+		public string Name { get; set; }
+		[DisplayName("Display Order")]
+		public int DisplayOrder { get; set; }
+	}
+}
