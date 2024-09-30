0ff9d75c7eff604b9745a9840de50d3a904c87f6
regmipreety <priety.regmi46@gmail.com>
2024-09-30 18:15:55 -0500

    Adding Repository file

---
+++ ASPWebApplication/Repository/Repository.cs
@@ -0,0 +1,35 @@
+﻿using ASPWebApplication.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace ASPWebApplication.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        
+
+        void IRepository<T>.Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        T IRepository<T>.Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<T>.RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
