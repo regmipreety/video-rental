538c5347b252277f92e1c9a00c1339ce8f28bff7
regmipreety <priety.regmi46@gmail.com>
2024-10-18 22:53:11 +1100

    Implementing Google Authentication

---
+++ ASPWebApplication/Areas/Customer/Controllers/OauthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWebApplication.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OauthController : Controller
+    {
+        public IActionResult Login(string returnUrl = "/")
+        {
+            // Challenge the authentication scheme (e.g., Google)
+            return Challenge(new AuthenticationProperties { RedirectUri = returnUrl }, "Google");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- ASPWebApplication/Program.cs
+++ ASPWebApplication/Program.cs
@@ -3,28 +3,46 @@ using ASPWebApplication.Repository;
 using ASPWebApplication.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add DbContext with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDbContext < ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))) ;
-
+// Add Identity service
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-//Add Category Repo and interface to the services
+
+// Add UnitOfWork service
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+// Add Razor Pages support
 builder.Services.AddRazorPages();
 
-// Add services to the container.
+// Add MVC support
 builder.Services.AddControllersWithViews();
+
+// Configure OAuth (Google in this example)
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = "";       // Add your Google OAuth Client ID
+    googleOptions.ClientSecret = ""; // Add your Google OAuth Client Secret
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -32,12 +50,15 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();
 
+// Enable authentication and authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
+// Map Razor Pages
 app.MapRazorPages();
 
+// Map the default controller route
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
